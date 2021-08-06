9057503e3a05f9f2947d859b0aa28cdac6171f8a
Qusay <qusayfadhel@gmail.com>
2021-08-06 10:08:45 +0200

    After assing Udacity review

--- project_template 3/plugins/operators/load_dimension.py
+++ project_template 3/plugins/operators/load_dimension.py
@@ -4,19 +4,45 @@ from airflow.utils.decorators import apply_defaults
 
 class LoadDimensionOperator(BaseOperator):
 
+    """
+    Extracts data from stagging tables and loads it into the dimenssion tables
+    
+        Parameters:
+            redshift_conn_id: Redshift conection id
+            table: Destination table where data will be loaded
+            sql: Select statment that will read data from a data source
+            truncate_insert: Defines whether old data should be removed or kept before loadin new data to the dimenssion table. This option is set to True by default
+            
+        Returns:
+            None
+    """
+    
     ui_color = '#80BD9E'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql="",
+                 truncate_insert=True,
                  *args, **kwargs):
 
         super(LoadDimensionOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table=table
+        self.sql=sql
+        self.truncate_insert=truncate_insert
 
     def execute(self, context):
-        self.log.info('LoadDimensionOperator not implemented yet')
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        #Check if the old data should be deleted before loading the exctracted data 
+        if truncate_insert:
+            self.log.info(f"Clearing data from {self.table} table")
+            redshift_hook.run(f"DELETE FROM {self.table}")
+
+        #Loading the extracted data into the dimenssion table
+        self.log.info("Inserting data into {self.table} table")
+        redshift_hook.run(f"""INSERT INTO {self.table}
+                                        {self.sql}
+                        """)
--- project_template 3/plugins/operators/load_fact.py
+++ project_template 3/plugins/operators/load_fact.py
@@ -4,19 +4,41 @@ from airflow.utils.decorators import apply_defaults
 
 class LoadFactOperator(BaseOperator):
 
+    """
+    Extracts data from stagging tables and loads it into the fact table
+    
+        Parameters:
+            redshift_conn_id: Redshift conection id
+            table: Destination table where data will be loaded
+            sql: Select statment that will read data from a data source
+            
+        Returns:
+            None
+    """
+    
     ui_color = '#F98866'
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # conn_id = your-connection-name
+                 redshift_conn_id="",
+                 table="",
+                 sql="",
                  *args, **kwargs):
 
         super(LoadFactOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.table=table
+        self.sql=sql
 
     def execute(self, context):
-        self.log.info('LoadFactOperator not implemented yet')
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info("Clearing data from fact table")
+        redshift_hook.run(f"DELETE FROM {self.table}")
+
+        self.log.info("Inserting data into fact table")
+        formatted_sql=f"""INSERT INTO {self.table}
+                                        {self.sql}
+                        """
+        
+        redshift_hook.run(formatted_sql)
